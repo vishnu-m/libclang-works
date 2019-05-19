3456d547316c2b64e90c717fc831237f9f73b5af
Vishnu M <vishnumurali7000@gmail.com>
2019-05-19 17:32:37 +0530

    Formatted code

--- main.cpp
+++ main.cpp
@@ -9,19 +9,22 @@ std::string Convert(const CXString& s)
    return result;
 }
 
-CXChildVisitResult visitor(CXCursor cursor, CXCursor, CXClientData) {
-  CXCursorKind kind = clang_getCursorKind(cursor);
-  CXString kindName = clang_getCursorKindSpelling( kind );
-  CXCursor parent = clang_getCursorLexicalParent (cursor);
-  auto type = clang_getCursorType(cursor);
-  auto typeStr  = Convert(clang_getTypeSpelling(type));
-  std::string result = clang_getCString( kindName );
-  std::string parentNameStr = clang_getCString(clang_getCursorDisplayName(parent));
 
-  CXString cursorSpelling = clang_getCursorSpelling( cursor );
-  std::string curSpellingStr = clang_getCString( cursorSpelling );
-  std::cout << typeStr <<std::endl;
-  //std::cout<< curSpellingStr <<"--------" << result<<" >>>>>> " << parentNameStr << typeStr << std::endl;
+CXChildVisitResult visitor(CXCursor cursor, CXCursor, CXClientData) {
+  CXCursorKind cursor_kind = clang_getCursorKind(cursor);
+  CXString cursor_kind_name = clang_getCursorKindSpelling(cursor_kind);
+  std::string cursor_kind_str = Convert(cursor_kind_name);
+
+  CXString cursor_spelling = clang_getCursorSpelling(cursor);
+  std::string cursor_spelling_str = Convert(cursor_spelling);
+
+  CXType cursor_type = clang_getCursorType(cursor);
+  std::string cursor_type_str  = Convert(clang_getTypeSpelling(cursor_type));
+
+  CXCursor parent = clang_getCursorLexicalParent(cursor);
+  std::string parent_name_str = Convert(clang_getCursorDisplayName(parent));
+
+  std::cout<< cursor_spelling_str <<"--------" << cursor_kind_str<<" >>>>>> " << parent_name_str << cursor_type_str << std::endl;
   return CXChildVisit_Recurse;
 }
 
--- test.c
+++ test.c
@@ -1,5 +1,3 @@
 enum week{Mon, Tue, Wed, Thur, Fri, Sat, Sun};
-struct Distance{
-  int time;
-  int day;
-}d1;
+enum week day;
+day = Wed;
