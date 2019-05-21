8511a1f837b412144028733de654bd0d26eab09e
Vishnu M <vishnumurali7000@gmail.com>
2019-05-21 21:13:10 +0530

    Completed enumerations

--- main.cpp
+++ main.cpp
@@ -1,38 +1,62 @@
 #include <clang-c/Index.h>
-
 #include <iostream>
 
-std::string Convert(const CXString& s)
+std::string convert(const CXString& s)
 {
    std::string result = clang_getCString(s);
    clang_disposeString(s);
    return result;
 }
 
-
-CXChildVisitResult visitor(CXCursor cursor, CXCursor, CXClientData) {
+std::string get_cursor_kind(CXCursor cursor){
   CXCursorKind cursor_kind = clang_getCursorKind(cursor);
   CXString cursor_kind_name = clang_getCursorKindSpelling(cursor_kind);
-  std::string cursor_kind_str = Convert(cursor_kind_name);
+  std::string cursor_kind_str = convert(cursor_kind_name);
+  return cursor_kind_str;
+}
 
-  CXString cursor_spelling = clang_getCursorSpelling(cursor);
-  std::string cursor_spelling_str = Convert(cursor_spelling);
-
+std::string get_cursor_type(CXCursor cursor){
   CXType cursor_type = clang_getCursorType(cursor);
-  std::string cursor_type_str  = Convert(clang_getTypeSpelling(cursor_type));
+  std::string cursor_type_str  = convert(clang_getTypeSpelling(cursor_type));
+  return cursor_type_str;
+}
 
+std::string get_cursor_spelling(CXCursor cursor){
+  CXString cursor_spelling = clang_getCursorSpelling(cursor);
+  std::string cursor_spelling_str = convert(cursor_spelling);
+  return cursor_spelling_str;
+}
+
+std::string get_parent(CXCursor cursor){
   CXCursor parent = clang_getCursorLexicalParent(cursor);
-  std::string parent_name_str = Convert(clang_getCursorDisplayName(parent));
+  std::string parent_name_str = convert(clang_getCursorDisplayName(parent));
+  return parent_name_str;
+}
 
-  //std::cout<< cursor_spelling_str <<"--------" << cursor_kind_str<<" >>>>>> " << parent_name_str << cursor_type_str << std::endl;
-  if(cursor_kind_str == "EnumDecl"){
-    std::cout << "EnumDecl" << "   " << cursor_spelling_str << '\n';
+CXChildVisitResult enum_visitor(CXCursor cursor, CXCursor parent, CXClientData) {
+  if(get_cursor_kind(cursor) == "IntegerLiteral"){
+    long long enum_const_value = clang_getEnumConstantDeclValue(parent);
+    std::cout << "IntegerLiteral" << "   " << enum_const_value << "  Parent = " << get_cursor_spelling(parent) << std::endl;
   }
-
-  else if(cursor_kind_str == "EnumConstantDecl"){
-    std::cout << "   " << "EnumConstantDecl" << "   " << cursor_spelling_str << " --- parent = " << parent_name_str << '\n';
+  else if(get_cursor_kind(cursor) == "EnumConstantDecl"){
+    long long enum_const_value = clang_getEnumConstantDeclValue(cursor);
+    std::cout << "EnumConstantDecl" << "   " << get_cursor_spelling(cursor) << "  Parent = " << get_parent(cursor) << std::endl;
   }
+  
   return CXChildVisit_Recurse;
+}
+
+void enum_handler(CXCursor cursor){
+  std::cout << "EnumDecl" << "   " << get_cursor_spelling(cursor) << std::endl;
+  clang_visitChildren(cursor, enum_visitor, 0);
+}
+
+CXChildVisitResult visitor(CXCursor cursor, CXCursor, CXClientData) {
+  if(get_cursor_kind(cursor) == "EnumDecl"){
+    enum_handler(cursor);
+  }
+ 
+  return CXChildVisit_Continue;
 }
 
 int main(int argc, char **argv) {
--- test.c
+++ test.c
@@ -1,2 +1,2 @@
-enum week{Mon, Tue, Wed, Thur, Fri, Sat, Sun};
+enum week{Mon=99, Tue, Wed, Thur, Fri, Sat, Sun};
 enum colours{Red, Black, Yellow};
